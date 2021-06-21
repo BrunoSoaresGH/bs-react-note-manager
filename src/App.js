6da8203c20685331cedab14283398965a3b59a51
BrunoSoaresGH <brunosoaresbsv@gmail.com>
2021-06-21 19:24:21 -0300

    NoteList, NoteForm and NoteCard components created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-function App() {
-  return (
-    <h1>BS React Note Manager</h1>
-  );
+import { Component } from "react";
+import NoteForm from "./components/NoteForm";
+import NoteList from "./components/NoteList";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <NoteForm />
+        <NoteList />
+      </section>
+    );
+  }
 }
 
 export default App;
